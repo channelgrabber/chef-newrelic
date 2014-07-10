0a69bc44c7608bb1d577ecf7e0609860929b22e3
Andu <andua@channelgrabber.com>
2014-07-10 15:38:04 +0100

    Another revert.

--- recipes/meetme-plugin.rb
+++ recipes/meetme-plugin.rb
@@ -34,7 +34,9 @@ files.each do |file|
 end
 
 services_yml = nil
-services = node['newrelic']['meetme-plugin']['services']
+services = {
+  '#services' => node['newrelic']['meetme-plugin']['services']
+}
 
 unless services.nil?
   require 'yaml'
