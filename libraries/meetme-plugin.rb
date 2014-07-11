5615d997a1b495363268b332cd14ef8a468c2ae5
Andu <andua@channelgrabber.com>
2014-07-11 17:39:29 +0100

    CGIV-2548-Meetme-Newrelic-Plugin fix mash problem

--- libraries/meetme-plugin.rb
+++ libraries/meetme-plugin.rb
@@ -1,13 +1,16 @@
 def convertChefMashToHash(mashrray)
-	puts "###/////////////////////////////////////////////////////////////////////////////" 
-	puts "Aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" 
-	puts "///////////////////////////////////////////////////////////////////////////////"
 	if mashrray.kind_of?(::Chef::Node::ImmutableMash)
+		puts "###/////////////////////////////////////////////////////////////////////////////" 
+		puts "Aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" 
+		puts "///////////////////////////////////////////////////////////////////////////////"
 		mashrray = mashrray.to_hash
 		mashrray.each do |key, value|
 			convertChefMashToHash(value)
 		end
 	elsif mashrray.kind_of?(::Chef::Node::ImmutableArray)
+		puts "###/////////////////////////////////////////////////////////////////////////////" 
+		puts "Aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" 
+		puts "///////////////////////////////////////////////////////////////////////////////"
 		mashrray = mashrray.to_a
 		mashrray.each_index do |index|
 			convertChefMashToHash(mashrray[index])
