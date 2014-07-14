58b64916461a6779c18248b9c03fe17c2a46879e
Andu <andua@channelgrabber.com>
2014-07-14 09:22:16 +0100

    CGIV-2548-Meetme-Newrelic-Plugin fix mash problem

--- libraries/meetme-plugin.rb
+++ libraries/meetme-plugin.rb
@@ -1,19 +1,29 @@
 def convertChefMashToHash(mashrray)
 	if mashrray.kind_of?(::Chef::Node::ImmutableMash)
-		puts "###/////////////////////////////////////////////////////////////////////////////" 
-		puts "Aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" 
-		puts "///////////////////////////////////////////////////////////////////////////////"
-		mashrray = mashrray.to_hash
+		mashrray = fromImmutableMashToHash(mashrray)
 		mashrray.each do |key, value|
 			convertChefMashToHash(value)
 		end
 	elsif mashrray.kind_of?(::Chef::Node::ImmutableArray)
-		puts "###/////////////////////////////////////////////////////////////////////////////" 
-		puts "Aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" 
-		puts "///////////////////////////////////////////////////////////////////////////////"
-		mashrray = mashrray.to_a
+		mashrray = fromImmutableArrayToArray(mashrray)
 		mashrray.each_index do |index|
 			convertChefMashToHash(mashrray[index])
 		end
 	end
+end
+
+def fromImmutableArrayToArray(array)
+	new_array = Array.new(array.size)
+	array.each_index do |index|
+		new_array[index] = array[index]
+	end
+	return new_array
+end
+
+def fromImmutableMashToHash(mash)
+	new_hash = Hash.new
+	mash.each do |key, value|
+		new_hash[key] = value
+	end
+	return new_hash
 end
