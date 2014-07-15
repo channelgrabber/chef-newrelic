d3fc32ec1a8f4a3cab4aa4df879a084b96645bab
Andu <andua@channelgrabber.com>
2014-07-15 17:12:36 +0100

    Try cleaning the library code

--- libraries/meetme-plugin.rb
+++ libraries/meetme-plugin.rb
@@ -1,12 +1,14 @@
 def convertImmutableMashToHash(mashrray)
 	if mashrray.kind_of?(::Chef::Node::ImmutableMash)
-		mashrray = fromImmutableMashToHash(mashrray)
+		#mashrray = fromImmutableMashToHash(mashrray)
+		mashrray = mashrray.to_hash
 		mashrray.each do |key, value|
 			mashrray[key] = convertImmutableMashToHash(value)
 		end
 		return mashrray
 	elsif mashrray.kind_of?(::Chef::Node::ImmutableArray)
-		mashrray = fromImmutableArrayToArray(mashrray)
+		mashrray = #fromImmutableArrayToArray(mashrray)
+		mashrray = mashrray.to_a
 		mashrray.each_index do |index|
 			mashrray[index] = convertImmutableMashToHash(mashrray[index])
 		end
