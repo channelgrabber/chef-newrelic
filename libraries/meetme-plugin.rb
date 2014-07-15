ebf7b004330d6c33a6993d08abc234c204145b4a
Andu <andua@channelgrabber.com>
2014-07-15 17:17:46 +0100

    Remove unused code from libraries

--- libraries/meetme-plugin.rb
+++ libraries/meetme-plugin.rb
@@ -1,13 +1,11 @@
 def convertImmutableMashToHash(mashrray)
 	if mashrray.kind_of?(::Chef::Node::ImmutableMash)
-		#mashrray = fromImmutableMashToHash(mashrray)
 		mashrray = mashrray.to_hash
 		mashrray.each do |key, value|
 			mashrray[key] = convertImmutableMashToHash(value)
 		end
 		return mashrray
 	elsif mashrray.kind_of?(::Chef::Node::ImmutableArray)
-		mashrray = #fromImmutableArrayToArray(mashrray)
 		mashrray = mashrray.to_a
 		mashrray.each_index do |index|
 			mashrray[index] = convertImmutableMashToHash(mashrray[index])
@@ -15,20 +13,4 @@ def convertImmutableMashToHash(mashrray)
 		return mashrray
 	end
 	return mashrray
-end
-
-def fromImmutableArrayToArray(array)
-	new_array = Array.new(array.size)
-	array.each_index do |index|
-		new_array[index] = array[index]
-	end
-	return new_array
-end
-
-def fromImmutableMashToHash(mash)
-	new_hash = Hash.new
-	mash.each do |key, value|
-		new_hash[key] = value
-	end
-	return new_hash
 end
