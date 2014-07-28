fa8cffd8b43d0ae0b08729a11e24b916567ceeb3
Andu Aanei <alexandru.aanei@gmail.com>
2014-07-28 13:45:55 +0100

    Try another fix for missing php-fpm config

--- attributes/php-agent.rb
+++ attributes/php-agent.rb
@@ -14,11 +14,9 @@ if node.attribute?('php')
 end
 
 if !node.recipe?("php-fpm")
-  return
+  default['newrelic']['php-agent']['install_silently'] = false
+  default['newrelic']['php-agent']['startup_mode'] = 'agent'
+  default['newrelic']['php-agent']['web_server']['service_name'] = 'nginx'
+  default['newrelic']['php-agent']['php_recipe'] = 'php::default'
+  default['newrelic']['php-agent']['config_file'] = "#{node['php-fpm']['conf_dir']}/conf.d/newrelic.ini"
 end
-
-default['newrelic']['php-agent']['install_silently'] = false
-default['newrelic']['php-agent']['startup_mode'] = 'agent'
-default['newrelic']['php-agent']['web_server']['service_name'] = 'nginx'
-default['newrelic']['php-agent']['php_recipe'] = 'php::default'
-default['newrelic']['php-agent']['config_file'] = "#{node['php-fpm']['conf_dir']}/conf.d/newrelic.ini"
